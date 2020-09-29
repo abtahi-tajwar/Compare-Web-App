398f1a8aa4ccac62d98ffd347c8550b0802299b3
abtahi-tajwar <abtahitajwar@gmail.com>
2020-09-29 21:48:23 +0600

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SideNav from './Components/SideNav'
+
+function App() {
+  return (
+    <div className="App">
+      <SideNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SideNav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SideNav  = () => {
+    return (
+        <div className="sidenav">
+             <ul className="sidenav_menu">
+                 <li>home</li>
+                 <li>My Forms</li>
+                 <li>Added Cards</li>
+                 <li>Compare</li>
+                 <li>About Us</li>
+             </ul>
+        </div>
+    )
+}
+
+export default SideNav
